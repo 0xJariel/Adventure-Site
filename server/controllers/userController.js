846188e11ba95925cbd8a64359c6516c5a21fe43
J <0xjariel@protonmail.com>
2023-09-11 03:00:57 -0600

    user signup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,17 +1,12 @@
 const mongoose = require("mongoose");
-const Adventure = require("../models/adventure");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
 const createNewUser = async (req, res, next) => {
   try {
     console.log(req.body);
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
-    });
-    const result = await user.save();
-    res.redirect("/");
+    const user = await User.create({ ...req.body });
+    res.status(201).json(user);
   } catch (err) {
     return next(err);
   }
